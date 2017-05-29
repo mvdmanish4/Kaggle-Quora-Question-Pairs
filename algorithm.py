39f6436bd03fe496d20cc5605777f524b54fee27
Karthik <gvkarthik93@gmail.com>
2017-05-29 19:51:02 +0530

    Modified classifier

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,41 @@
-import tensorflow.contrib.learn as skflow
-from sklearn import datasets, metrics
+#Decision Tree Classifier
+import sklearn
+from sklearn import tree
+features = [[140,"smooth"],[130,"smooth"],
+[150,"bumpy"],[170,"bumpy"]]
 
-#load dataset here from the csv file
-trainingData = loadData()
-classfier = skflow.TensorFlowLinearClassifier(n_classes=4)
-classifier.fit(trainingData.features, trainingData.target)
+#140 is the feature such as term frequency
+#smooth = 1 (smooth is the question)
+#bumpy = 0 (bumpy is another question)
+
+binFeatures = [[140,1],[130,1],
+[150,0],[170,0]]
+
+labels = ["a","a","b","b"]
+
+#a = 1
+#b = 0
+
+binLabels = [1,1,0,0]
+
+#Classifier Object
+clf = tree.DecisionTreeClassifier()
+
+#Training algorithm is included in classifier object and is called fit
+#Think of fit as synonym for "Find Patterns in Data"
+clf = clf.fit(binFeatures,binLabels)
+
+#Input to the classifier is the feature of new fruit
+#You can give any number of inputs in classifier
+print (clf.predict([[150,0]]))
+if clf.predict([[150,0]]) == 1:
+	print ("a")
+else:
+	print ("b")
+
+print (type(clf.predict([[150,0]])))
+
+#Learn how to set features
+#Learn numpy and scikit learn libraries
+#Implement learning algorithm by storing input data in training set after processing
+C
